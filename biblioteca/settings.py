a822a397d27746822e34c6f9d38522c7b72c9881
Gutis007 <gutisilva123@hotmail.com>
2024-10-03 18:23:24 -0300

    Inivio da configuração dos filtros

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -28,8 +28,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'django_filters',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
